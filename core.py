ec76e4f99e300319c503acdde91fce267f849d47
Aedan Cullen <aedan@pig.vision>
2021-07-27 23:52:47 -0500

    Set chunk from params, add multiplier, revert some things, add rospy

--- core.py
+++ core.py
@@ -1,6 +1,10 @@
 import time
 import math
 
+import rospy
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+
 import numpy as np
 from ompl import util as ou
 from ompl import base as ob
@@ -13,13 +17,18 @@ import util
 PARAMS = {'mu': 1.0489, 'C_Sf': 4.718, 'C_Sr': 5.4562, 'lf': 0.15875, 'lr': 0.17145, 'h': 0.074, 'm': 3.74, 'I': 0.04712, 's_min': -0.4189, 's_max': 0.4189, 'sv_min': -3.2, 'sv_max': 3.2, 'v_switch': 7.319, 'a_max': 9.51, 'v_min':-5.0, 'v_max': 20.0, 'width': 0.31, 'length': 0.58}
 
 NUM_CONTROLS = 2
-CONTROL_LOWER = [0.0, 0.0]
-CONTROL_UPPER = [1.0, 1.0]
+CONTROL_LOWER = [PARAMS["s_min"], PARAMS["v_min"]]
+CONTROL_UPPER = [PARAMS["s_max"], PARAMS["v_max"]]
 
 BIASMAP_XY_SUBDIV = 10
-BIASMAP_YAW_SUBDIV = 10
+BIASMAP_YAW_SUBDIV = 20
 
-CHUNK_DURATION = 0.100
+PHYSICS_TIMESTEP = 0.01 # Actual value used in calculation
+SIM_INTERVAL = 0.02 # Real time interval of simulator's internal physics callbacks
+
+CHUNK_MULTIPLIER = 10
+
+CHUNK_DURATION = SIM_INTERVAL * CHUNK_MULTIPLIER
 CHUNK_DISTANCE = 10
 
 class CourseProgressGoal(ob.GoalState):
@@ -47,7 +56,7 @@ class TimestepOptimizationObjective(ob.OptimizationObjective):
         self.si = si
 
     def motionCost(self, s1, s2):
-        return 1# + self.si.distance(s1, s2)
+        return 1 + self.si.distance(s1, s2)
 
 class BiasmapControlSampler(oc.ControlSampler):
     def __init__(self, controlspace, biasmap, biasmap_valid):
@@ -55,24 +64,31 @@ class BiasmapControlSampler(oc.ControlSampler):
         self.controlspace = controlspace
         self.biasmap = biasmap
         self.biasmap_valid = biasmap_valid
+        self.exact_flags = np.ones_like(biasmap, dtype=bool)
 
     def sample(self, control, start_state):
         bi_x = round(start_state[0].getX() * BIASMAP_XY_SUBDIV)
         bi_y = round(start_state[0].getY() * BIASMAP_XY_SUBDIV)
         bi_yaw = round((start_state[0].getYaw() + math.pi) * BIASMAP_YAW_SUBDIV / (2 * math.pi))
-        result_data = self.biasmap[bi_x, bi_y, bi_yaw, :, :]
+        result_data = self.biasmap[bi_x, bi_y, bi_yaw, :]
         result_valid = self.biasmap_valid[bi_x, bi_y, bi_yaw]
+        result_exact = self.exact_flags[bi_x, bi_y, bi_yaw, :]
         if result_valid:
-            for i in range(NUM_CONTROLS):
-                cvalue = np.random.normal(result_data[i, 0], result_data[i, 1])
-                cvalue = np.clip(cvalue, CONTROL_LOWER[i], CONTROL_UPPER[i])
-                control[i] = cvalue
+            if result_exact[0]:
+                result_exact[0] = False
+                for i in range(NUM_CONTROLS):
+                    control[i] = np.clip(result_data[i], CONTROL_LOWER[i], CONTROL_UPPER[i])
+            else:
+                for i in range(NUM_CONTROLS):
+                    cvalue = np.random.normal(result_data[i], BIASMAP_CONTROL_STDEV[i])
+                    cvalue = np.clip(cvalue, CONTROL_LOWER[i], CONTROL_UPPER[i])
+                    control[i] = cvalue
         else:
             for i in range(NUM_CONTROLS):
                 control[i] = np.random.uniform(CONTROL_LOWER[i], CONTROL_UPPER[i])
 
 class QCPlan1:
-    def __init__(self, waypoints_fn, gridmap_fn, biasmap_fn):
+    def __init__(self, agent_name, waypoints_fn, gridmap_fn, biasmap_fn):
         self.waypoints = np.loadtxt(waypoints_fn, delimiter=',', dtype=np.float32)
         self.gridmap = np.load(gridmap_fn)
         biasmap_data = np.load(biasmap_fn)
@@ -80,7 +96,7 @@ class QCPlan1:
         self.biasmap_valid = biasmap_data["biasmap_valid"]
 
         self.se2space = ob.SE2StateSpace()
-        self.vectorspace = ob.RealVectorStateSpace(4)
+        self.vectorspace = ob.RealVectorStateSpace(5)
 
         self.statespace = ob.CompoundStateSpace()
         self.statespace.addSubspace(self.se2space, 1)
@@ -101,12 +117,23 @@ class QCPlan1:
         self.ss.setPlanner(self.planner)
 
         self.ss.getProblemDefinition().setOptimizationObjective(TimestepOptimizationObjective(self.si))
-
+        
+        self.last_controls = None
+        self.last_state = None
+        self.last_scan = None
 
         self.provided_car = util.RaceCar(PARAMS, 12345) # seed garbage not used; nobody cares
 
     def loop(self):
-
+        # On timer:
+        
+        # Issue prepared controls
+        # Compute next position after chunk duration
+        # Get latest scan, setup and plan for next chunk
+        # Save prepared controls
+        
+    def lidar_callback(self):
+        
 
     def state_validity_check(self, state):
         return self.si.satisfiesBounds(state)
@@ -183,12 +210,8 @@ def plan():
         # print the path to screen
         print("Found solution:\n%s" % ss.getSolutionPath().printAsMatrix())
 
-
 if __name__ == "__main__":
-    plan()
-    s = time.time()
-    plan()
-    print(time.time() - s)
-    #while True:
-    #    time.sleep(0.100)
-    #    print("yo")
+    qc = QCPlan1()
+    loop_timer = rospy.Timer(rospy.Duration(CHUNK_DURATION), qc.loop)
+    scan_sub = rospy.Subscriber("/%s/scan" % self.agent_name, LaserScan, qc.lidar_callback, queue_size=1)
+    rospy.spin()
