858b9a703e2d90a2c2358cce15b7d7991dc948c9
Aedan Cullen <aedan@pig.vision>
2021-09-22 09:45:25 -0500

    Conservative

--- f1tenth_ros_agent.py
+++ f1tenth_ros_agent.py
@@ -33,12 +33,12 @@ SIM_INTERVAL = 0.02 # Real time interval of simulator's internal physics callbac
 CHUNK_MULTIPLIER = 5
 
 CHUNK_DURATION = SIM_INTERVAL * CHUNK_MULTIPLIER
-CHUNK_DISTANCE = 7
+CHUNK_DISTANCE = 5
 GOAL_THRESHOLD = 2
 
 HEURISTIC_DIRECTION_STEP = np.radians(1)
-HEURISTIC_CONT_THRESH = 2
-STEER_GAIN = 0.3
+HEURISTIC_CONT_THRESH = 1
+STEER_GAIN = 0.2
 STEER_STDEV = 0.2
 VEL_MEAN_L = 10
 VEL_MEAN_H = 15
@@ -55,28 +55,28 @@ class QCPassControlSampler(oc.ControlSampler):
     def sample(self, control, state, selections):
         np_state = np.array([state[0].getX(), state[0].getY(), state[0].getYaw()])
 
-        target, goal_direction = util.tangent_bug(
-            np_state,
-            self.latched_map,
-            GRIDMAP_XY_SUBDIV,
-            self.goal_point,
-            self.goal_angle,
-            HEURISTIC_DIRECTION_STEP,
-            HEURISTIC_CONT_THRESH,
-            0.001#PARAMS["width"],
-        )
-
-        front_dist = util.rangefind(np_state, self.latched_map, GRIDMAP_XY_SUBDIV, np_state[2], 100, PARAMS["width"])
-
-        #target, front_dist = util.farthest_target(
+        #target, goal_direction = util.tangent_bug(
             #np_state,
             #self.latched_map,
             #GRIDMAP_XY_SUBDIV,
             #self.goal_point,
             #self.goal_angle,
             #HEURISTIC_DIRECTION_STEP,
+            #HEURISTIC_CONT_THRESH,
             #0.001#PARAMS["width"],
         #)
+
+        #front_dist = util.rangefind(np_state, self.latched_map, GRIDMAP_XY_SUBDIV, np_state[2], 100, 0.001)
+
+        target, front_dist = util.farthest_target(
+            np_state,
+            self.latched_map,
+            GRIDMAP_XY_SUBDIV,
+            self.goal_point,
+            self.goal_angle,
+            HEURISTIC_DIRECTION_STEP,
+            0.001#PARAMS["width"],
+        )
 
         #c0 = np.random.normal(np.clip(target * STEER_GAIN, CONTROL_LOWER[0], CONTROL_UPPER[0]), STEER_STDEV)
         #c1 = np.random.normal(np.clip(best_dist * VEL_GAIN, CONTROL_LOWER[1], CONTROL_UPPER[1]), VEL_STDEV)
@@ -85,7 +85,7 @@ class QCPassControlSampler(oc.ControlSampler):
 
         control[0] = np.clip(np.random.normal(target * STEER_GAIN, STEER_STDEV), CONTROL_LOWER[0], CONTROL_UPPER[0])
         #control[1] = np.clip(np.random.normal(best_dist * VEL_GAIN, VEL_STDEV), CONTROL_LOWER[1], CONTROL_UPPER[1])
-        c1 = np.random.normal(np.clip(front_dist * VEL_GAIN, CONTROL_UPPER[1] / 2, CONTROL_UPPER[1]), VEL_STDEV)
+        c1 = np.random.normal(np.clip(front_dist * VEL_GAIN, CONTROL_UPPER[1] / 4, CONTROL_UPPER[1]), VEL_STDEV)
         control[1] = np.clip(c1, CONTROL_LOWER[1], CONTROL_UPPER[1])
 
         #if best_dist > 10:
